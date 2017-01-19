f95beb679956fa2f0280e79edae0f7d7ce95e9e0
Donjohn <donjohn.geekzone@gmail.com>
2017-01-19 16:03:24 +0100

    fix remove emdia 2eme

--- Generator/DoctrineCrudGenerator.php
+++ Generator/DoctrineCrudGenerator.php
@@ -199,8 +199,7 @@ class DoctrineCrudGenerator extends Generator
         $parts = explode('\\', $this->entity);
         $entityClass = array_pop($parts);
         $entityNamespace = implode('\\', $parts);
-
-        $dir = $this->bundle->getPath().'/Tests/Controller';
+        $dir = $this->rootDir.'/../tests/'.str_replace('\\', '/', $this->bundle->getName()).'/Controller';
         $target = $dir.'/'.str_replace('\\', '/', $entityNamespace).'/'.$entityClass.'ControllerTest.php';
 
         $this->renderFile('crud/tests/test.php.twig', $target, array(
